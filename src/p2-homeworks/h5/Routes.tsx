dc1fe9a331396e08c2f523bc468c90239d08efda
DiamanDD <Dimond@list.ru>
2021-08-26 17:57:08 +0300

    comi

--- src/p2-homeworks/h5/Routes.tsx
+++ src/p2-homeworks/h5/Routes.tsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import {Redirect, Switch, Route} from "react-router-dom"
+import PreJunior from "./pages/PreJunior";
+import Error404 from './pages/Error404';
+import Junior from './pages/Junior';
 
 export const PATH = {
     PRE_JUNIOR: '/pre-junior',
@@ -9,19 +13,20 @@ function Routes() {
     return (
         <div>
             {/*Switch выбирает первый подходящий роут*/}
-            {/*<Switch>*/}
+            <Switch>
 
             {/*в начале мы попадаем на страницу '/' и переходим сразу на страницу PRE_JUNIOR*/}
             {/*exact нужен чтоб указать полное совподение (что после '/' ничего не будет)*/}
             {/*<Route path={'/'} exact render={() => <Redirect to={PATH.PRE_JUNIOR}/>}/>*/}
 
-            {/*<Route path={PATH.PRE_JUNIOR} render={() => <PreJunior/>}/>*/}
+            <Route path={"/prejunior"} render={() => <PreJunior/>}/>
+                <Route path={"/junior"} render={() => <Junior/>}/>
                 // add routes
 
             {/*у этого роута нет пути, он отрисуется если пользователь захочет попасть на несуществующую страницу*/}
-            {/*<Route render={() => <Error404/>}/>*/}
+            <Route render={() => <Error404/>}/>
 
-            {/*</Switch>*/}
+            </Switch>
         </div>
     )
 }
---
+++ src/p2-homeworks/h5/pages/Components/Header/Header.tsx
@@ -0,0 +1,23 @@
+import React from "react"
+import {NavLink} from "react-router-dom"
+import style from "./Header.module.css"
+
+function Header() {
+    return (
+        <>
+        <div  className={style.buttonmenu}>menu
+            <div className={style.menu}>
+
+                <NavLink to={"/prejunior"} className={style.prejun}>Preunior</NavLink>
+                <NavLink to={"/junior"} className={style.jun}>Junior</NavLink>
+                <NavLink to={""} className={style.junplus}>Junior+</NavLink>
+
+
+            </div>
+        </div>
+
+        </>
+    )
+}
+
+export default Header
