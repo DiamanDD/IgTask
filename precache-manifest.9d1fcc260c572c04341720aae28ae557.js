49a700001ccdaccc13e04fe0d4eaa419f9074b08
DiamanDD <Dimond@list.ru>
2021-08-26 12:51:59 +0300

    Updates

---
+++ precache-manifest.9d1fcc260c572c04341720aae28ae557.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed66a2c41ac162f489c400cc71a1d0d",
+    "url": "/IgTask/index.html"
+  },
+  {
+    "revision": "89e0cf30af1937a6025e",
+    "url": "/IgTask/static/css/main.55f4e571.chunk.css"
+  },
+  {
+    "revision": "bc07ed2909983982f3e0",
+    "url": "/IgTask/static/js/2.fbd2b52b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/IgTask/static/js/2.fbd2b52b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89e0cf30af1937a6025e",
+    "url": "/IgTask/static/js/main.2a3757d1.chunk.js"
+  },
+  {
+    "revision": "bf4acc945371de165a40",
+    "url": "/IgTask/static/js/runtime-main.72dc93ae.js"
+  }
+]);
